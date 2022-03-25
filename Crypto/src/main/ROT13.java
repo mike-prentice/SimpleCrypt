4eed5ba600a74d0c816c5a40af0f97e2cd06db57
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-25 11:36:48 -0400

    added shift, cleaned up code

--- Crypto/src/main/ROT13.java
+++ Crypto/src/main/ROT13.java
@@ -36,33 +36,29 @@ public class ROT13  {
             }  else if (c >= 'N' && c <= 'Z'){
                 c -= 13;
             }
+            System.out.println(sb);
             sb.append(c);
         }
 
-        return text;
+        return sb.toString();
     }
 
     public String decrypt(String text) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < text.length(); i++){
-            char c = text.charAt(i);
-            if(c >= 'a' && c <= 'm'){
-                c += 13;
-            } else if (c >= 'A' && c <= 'M'){
-                c += 13;
-            }  else if (c >= 'n' && c <= 'z'){
-                c -= 13;
-            }  else if (c >= 'N' && c <= 'Z'){
-                c -= 13;
-            }
-            sb.append(c);
-        }
-        return text;
+
+      return  encrypt(text);
     }
 
     public static String rotate(String s, Character c) {
+        int shift = c - 'A';
+        StringBuilder sb = new StringBuilder();
+        char ch = 0;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.toLowerCase().charAt(i);
+            ch += shift;
+            sb.append(ch);
+        }
 
-        return "";
+        return String.valueOf(sb);
     }
 
 }
